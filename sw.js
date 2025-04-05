31feec88786190bdc32c18cdc13f09b7d6891711
Adrian12Rmz <105807408+Adrian12Rmz@users.noreply.github.com>
2025-04-04 22:40:02 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'pwa-crud-final-v3';
+const CACHE_NAME = 'pwa-crud-v4';
 const ASSETS = [
   '/pwa-crud-tareas/',
   '/pwa-crud-tareas/index.html',
   '/pwa-crud-tareas/style.css',
   '/pwa-crud-tareas/app.js',
-  '/pwa-crud-tareas/manifest.json'
+  '/pwa-crud-tareas/manifest.json',
+  '/pwa-crud-tareas/icon.png'
 ];
 
 self.addEventListener('install', (e) => {
@@ -12,13 +13,21 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
 });
